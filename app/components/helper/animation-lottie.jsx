3cf4685067b4f4d07ca8302dad7f87dc4eac4112
Dhyeybhuva2003 <dhyeybhuva2003@gmail.com>
2025-06-07 22:40:01 +0530

    Done

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,31 +1,19 @@
-'use client';
+"use client";
 
-import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
+import Lottie from "lottie-react";
 
-const Lottie = dynamic(() => import('lottie-react'), {
-  ssr: false,
-});
-
-const AnimationLottie = ({ animationPath, width = '95%' }) => {
-  const [animationData, setAnimationData] = useState(null);
-
-  useEffect(() => {
-    fetch(animationPath)
-      .then((res) => res.json())
-      .then((data) => setAnimationData(data))
-      .catch((err) => console.error('Failed to load animation:', err));
-  }, [animationPath]);
-
-  if (!animationData) return null; // Or a loading spinner
+const AnimationLottie = ({ animationPath, width }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationPath,
+    style: {
+      width: '95%',
+    }
+  };
 
   return (
-    <Lottie
-      animationData={animationData}
-      loop
-      autoplay
-      style={{ width }}
-    />
+    <Lottie {...defaultOptions} />
   );
 };
 
