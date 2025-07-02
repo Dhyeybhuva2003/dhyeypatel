d03ca52e2965b774889f72d0191ca455d2bece55
Dhyeyorionik1008 <dhyey.patel@orionik.com>
2025-07-02 22:23:52 +0530

    Change Done

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,14 +2,14 @@ export const experiences = [
   {
     id: 1,
     title: 'MERN Stack Developer',
-    company: "Orionik Technologies Pvt Ltd",
-    duration: "Jun 2025 - Present"
+    company: "Alliedge Technologies Pvt Ltd",
+    duration: "Mar 2025 - Jun 2025"
   },
   {
     id: 2,
     title: "Back End Developer",
     company: "Alliedge Technologies Pvt Ltd",
-    duration: "Jun 2024 - May 2025"
+    duration: "Jun 2024 - Mar 2025"
   },
   // {
   //   id: 3,
